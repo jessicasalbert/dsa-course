611bb621aa432327c1595533d6e7bbceca91e4cb
jessicasalbert <jessicarsalbert@gmail.com>
2021-02-01 09:51:42 -0500

    Implement get and set for hash

---
+++ hash.js
@@ -0,0 +1,36 @@
+class HashTable {
+    constructor(size=53){
+      this.keyMap = new Array(size);
+    }
+  
+    _hash(key) {
+      let total = 0;
+      let WEIRD_PRIME = 31;
+      for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96
+        total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+      }
+      return total;
+    }
+
+    set(key, val) {
+        let index = this._hash(key)
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [[key, val]]
+        } else {
+            this.keyMap[index].push([key, val])
+        }
+        return this
+    }
+
+    get(key) {
+        let index = this._hash(key)
+        if (!this.keyMap[index]) return false
+        let res = this.keyMap[index]
+        const arr = res.filter(arr => (arr[0] === key));
+        if (arr.length === 0) return false
+        return arr[0][1];
+    }
+  }
+
