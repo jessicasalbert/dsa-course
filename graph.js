afd4b4bbc21820ee1b02ce19487ce69586c492bb
jessicasalbert <jessicarsalbert@gmail.com>
2021-02-03 16:18:47 -0500

    Implement remove vertex for graph

--- graph.js
+++ graph.js
@@ -17,4 +17,11 @@ class Graph {
 
         this.adjacencyList[v2] = this.adjacencyList[v2].filter( v => v !== v1)
     }
+
+    removeVertex(vertex) {
+        for (const x of this.adjacencyList[vertex]) {
+            this.removeEdge(x, vertex)
+        }
+        delete this.adjacencyList[vertex] 
+    }
 }
